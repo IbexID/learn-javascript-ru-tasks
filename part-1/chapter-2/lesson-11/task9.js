1c116771238bdade8ba6f166c0c773d6ddb074c9
Roman <romquee@gmail.com>
2022-06-18 12:13:57 +0300

    p1/c2/l11 task9

---
+++ part-1/chapter-2/lesson-11/task9.js
@@ -0,0 +1,16 @@
+let userName = prompt('Введите имя', '');
+
+if (userName === 'Админ') {
+    let password = prompt('Введите пароль', '')
+    if (password === 'Я главный') {
+        alert('Здравствуйте!');
+    } else if (password === '' || password === null){
+        alert('Отменено');
+    } else {
+        alert('Неверный пароль')
+    }
+} else if (userName === '' || userName === null) {
+    alert( 'Отменено' );
+  } else {
+    alert( "Я вас не знаю" );
+  }
