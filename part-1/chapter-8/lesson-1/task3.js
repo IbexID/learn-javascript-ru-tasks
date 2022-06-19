580aed49ef32580a06e33ccd167f35565f6c8a56
Roman <romquee@gmail.com>
2022-06-19 13:42:59 +0300

    p1/c8/l1 task3

---
+++ part-1/chapter-8/lesson-1/task3.js
@@ -0,0 +1,13 @@
+let animal = {
+    eat() {
+      this.full = true;
+    }
+  };
+  
+  let rabbit = {
+    __proto__: animal
+  };
+  
+  rabbit.eat();
+
+// rabbit.full = true, поскольку this будет указывать на rabbit
