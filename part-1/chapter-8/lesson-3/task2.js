62d2d8a600acdad41e32a8b555a299b3e5b6d7d4
Roman <romquee@gmail.com>
2022-06-19 14:11:41 +0300

    p1/c8/l3 task2

---
+++ part-1/chapter-8/lesson-3/task2.js
@@ -0,0 +1,11 @@
+Function.prototype.defer = function (ms){
+    let f = this
+    return function(...args){
+        setTimeout(() =>f.apply(this, args), ms);
+    }
+}
+
+
+function f(a, b) {
+    alert( a + b );
+  }
